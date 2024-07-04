f47af4e03b8a470e80d3d763a799a260e637daee
Abhishek <ab2004ak@gmail.com>
2024-07-04 20:55:22 +0530

    added comment in limit.js

--- middlewares/limit.js
+++ middlewares/limit.js
@@ -2,6 +2,8 @@ import { Listing } from "../models/listing-model.js";
 import { Review } from "../models/review-model.js";
 import { ExpressErrors } from "../utils/errorHandlers.js";
 
+
+// limiting users to post to save storage from spamming or high costing 
 export const limitPosting = async( req, res, next)=>{
     let userId = req.user._id;
     let listingsCount = await Listing.countDocuments({owner:userId})
